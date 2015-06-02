f2d290e139bbbfd28d556ec7f43e8615a960a52d
Dan Minor & Peter <student@epicodus.com>
2015-06-02 10:50:36 -0700

    able to decrease price of movie based on time and age

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,30 @@
 //ticket attributes
 //time, new-release, age, name, price
 
-function Ticket(name, price){
+function Ticket(name, newRelease){
   this.name = name;
   this.time = [];
-  this.price= price;
+  this.price =  12;
+  this.newRelease = newRelease;
 }
+
+Ticket.prototype.setPrice = function(time, age) {
+  var newPrice = this.price;
+  if (age <= 12) {
+    newPrice -= 3;
+  }
+  else if (age >= 55) {
+    newPrice -= 2;
+  }
+  var timeArray = time.split("");
+  if (parseInt(timeArray[0]) < 1) {
+    newPrice -= 2;
+  }
+  else if (parseInt(timeArray[1]) < 8) {
+    newPrice -= 2;
+  }
+  return newPrice;
+};
 //inputtedTime = whatever
 //inputtedName = whatever
 //inputtedPrice = initial
--- spec/specs.js
+++ spec/specs.js
@@ -1,11 +1,41 @@
 describe("Ticket", function() {
+
+  it('Can determine price for matinee', function(){
+    var newTicket = new Ticket("Indiana Jones", 12, true);
+    expect(newTicket.setPrice("15:30", 18)).to.equal(10);
+  });
+  it('Can determine price for age', function() {
+    var newTicket = new Ticket("Indiana Jones", 12, true);
+    expect(newTicket.setPrice("18:00", 12)).to.equal(9);
+  })
+  it('Can determine price for age', function() {
+    var newTicket = new Ticket("Indiana Jones", 12, true);
+    expect(newTicket.setPrice("15:00", 12)).to.equal(7);
+  })
+
   it("Can create ticket object", function() {
     //name, time
-    //age, new-release and price as functions
-    var newTicket = new Ticket("Indiana Jones", 12);
+    //age, new-release, matinee and price as functions
+    var newTicket = new Ticket("Indiana Jones", true);
 
     expect(newTicket.name).to.equal("Indiana Jones");
     expect(newTicket.time).to.eql([]);
+    expect(newTicket.newRelease).to.eql(true);
     expect(newTicket.price).to.eql(12);
   });
+  // it("decrease price for age", function(){
+  //   var newTicket = new Ticket("Indiana Jones", 12, true);
+  //   expect(ticket.price())
+  // });
+
+  //everyday administrator will change whether or not it's a newrease manually
+
+  it("sets a time for a movie ticket", function()  {
+    var newTicket = new Ticket("Indiana Jones", 12, true);
+    newTicket.time.push("6:30");
+    newTicket.time.push("8:30");
+    expect(newTicket.time[0]).to.eql("6:30");
+    expect(newTicket.time[1]).to.eql("8:30");
+  })
+
 });
